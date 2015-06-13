ab2bc828b50efa14076a0263b12f933905b05188
tavistock <tavistock91@gmail.com>
2015-06-13 19:05:30 -0400

    broke up game of life and rendering stuff

---
+++ src/GameOfLife.js
@@ -0,0 +1,78 @@
+var Immutable = require('immutable');
+
+var rule = {BIRTH: 0,
+            DEATH: 1,
+            STASIS: 2};
+
+var Coord3 = Immutable.Record({a:0, b:0, c:0});
+
+function cells(faces) {
+  return Immutable.List(
+    faces.map((face) => {
+      return new Coord3({a: face.a, b: face.b, c: face.c});
+    }));
+}
+
+function blankBoard(cells) {
+  return Immutable.OrderedMap(cells.map(x => [x, 0]));
+}
+
+function randomBoard(cells) {
+  return Immutable.OrderedMap(cells.map(x => [x, Math.floor(2*Math.random())]));
+}
+
+function neighbors(cells, cutoff) {
+  return Immutable.OrderedMap(
+    cells.map(cell => [cell, neighborCells(cell, cells, cutoff)]));
+}
+
+function neighborCells(cell, allCells, cutoff){
+  // 1 shared is for inderect neighbors
+  // 2 shared is for direct neighbors
+  return allCells.map(neighborCell => {
+    var commonCoords = neighborCell
+    .valueSeq()
+    .filter(value => cell.contains(value))
+    .count();
+    if (commonCoords >= cutoff &&
+        commonCoords < 3) {
+      return neighborCell;
+    } else {
+      return false;
+    }})
+  .filterNot(x => x === false)
+}
+
+function step(state, neighbors, rules) {
+  return state.mapEntries(([cell, status]) => {
+    return [cell, stepCell(cell, status, state, neighbors, rules)];
+  });
+}
+
+function stepCell(cell, status, state, neighbors, rules){
+  var aliveNeighbors = neighbors.get(cell)
+  .map(neighbor => state.get(neighbor))
+  .reduce((x,y) => x + y);
+  return applyRules(status, aliveNeighbors, rules);
+}
+
+function applyRules(status, aliveNeighbors, rules) {
+  switch (rules[aliveNeighbors]) {
+      case rule.DEATH:
+        return 0;
+      case rule.BIRTH:
+        return 1;
+      case rule.STASIS:
+        return status;
+  }
+}
+
+module.exports = {
+  Coord3: Coord3,
+  rule: rule,
+  step: step,
+  cells: cells,
+  blankBoard: blankBoard,
+  randomBoard: randomBoard,
+  neighbors: neighbors
+}
--- src/index.js
+++ src/index.js
@@ -2,18 +2,104 @@ var Immutable = require('immutable');
 var THREE = require('three');
 var OrbitControls = require('three-orbit-controls')(THREE);
 
+var gol = require('./GameOfLife');
+
 var scene, camera, renderer, controls, container;
 var mesh;
-var cells, neighborsCache, rules;
+var board, neighborsCache;
+
+var rule = gol.rule;
+
+/// Parameter
+var rules = {
+  0: rule.DEATH,
+  1: rule.STASIS,
+  2: rule.BIRTH,
+  3: rule.DEATH,
+  4: rule.DEATH,
+  5: rule.DEATH,
+  6: rule.DEATH,
+  7: rule.DEATH,
+  8: rule.DEATH,
+  9: rule.DEATH,
+  10: rule.DEATH,
+  11: rule.DEATH,
+  12: rule.DEATH
+};
+var speed = 500;
+var subdivisions = 2;
+var neighborsCutoff = 1;
+
+// Beginning the loop
+
+init();
+animate();
+
+initGol();
+run();
+
+// Painting the board
 
 var clearColor = Math.random() * 0xffffff;
 var highlightColor = Math.random() * 0xffffff;
 var deadColor = Math.random() * 0xffffff;
-var aliveColor = Math.random() * 0xffffff;
+var aliveColor = deadColor * 0.90;
 
-// Beginning the loop
-init();
-animate();
+function paint(mesh, cells) {
+  var geo = mesh.geometry;
+  geo.faces.forEach((f) => {
+    if (cells.get(gol.Coord3({a:f.a, b:f.b, c:f.c})) > 0) {
+      f.color.setHex(aliveColor);
+    } else {
+      f.color.setHex(deadColor);
+    }
+  });
+  mesh.geometry.colorsNeedUpdate = true;
+}
+
+// Game controls
+
+var req;
+
+function run() {
+  step();
+  req = setTimeout(run, speed);
+}
+
+function stop() {
+  clearTimeout(req);
+}
+
+function random() {
+  cells = gol.randomBoard(gol.cells(mesh.geometry.faces));
+}
+
+function initGol(){
+  var faces = mesh.geometry.faces;
+  if (board === undefined) {
+    board = gol.randomBoard(gol.cells(faces));
+  }
+  if (neighborsCache === undefined) {
+    neighborsCache = Immutable.Map();
+  }
+  var neighborId = Immutable.List([faces.length, neighborsCutoff])
+  var neighbors = neighborsCache.get(neighborId)
+  if (neighbors === undefined) {
+    neighbors = gol.neighbors(gol.cells(faces), neighborsCutoff);
+    neighborsCache = neighborsCache.set(neighborId, neighbors);
+  }
+  paint(mesh, board);
+}
+
+function step() {
+  var neighborId = Immutable.List([mesh.geometry.faces.length,
+                                  neighborsCutoff])
+  var neighbors = neighborsCache.get(neighborId)
+  board = gol.step(board, neighbors, rules);
+  paint(mesh, board);
+}
+
+// THREE.js
 
 function init() {
   scene = new THREE.Scene();
@@ -43,7 +129,7 @@ function init() {
   light.position.set(10,50,100);
   scene.add(light);
   // CUSTOM
-  mesh = createMesh(1);
+  mesh = createMesh(subdivisions);
   scene.add(mesh);
 }
 
@@ -66,139 +152,19 @@ function render() {
 function createMesh(subdivisions) {
   var baseMat = new THREE.MeshBasicMaterial({color: 0xffffff,
                                             vertexColors: THREE.FaceColors});
-  var baseGeo = new THREE.IcosahedronGeometry(100,subdivisions);
+  var baseGeo = new THREE.IcosahedronGeometry(100, subdivisions);
   return new THREE.Mesh(baseGeo, baseMat);
 }
 
-// Coloring
-
-function paintRandom(mesh) {
-  var geo = mesh.geometry;
-  mesh.geometry.faces.forEach((f) => {
-    f.color.setRGB(Math.random(), Math.random(), Math.random());
-  });
-  mesh.geometry.colorsNeedUpdate = true;
-}
-
-function paint(mesh, cells) {
-  var geo = mesh.geometry;
-  geo.faces.forEach((f) => {
-    if (cells.get(Coord3({a:f.a, b:f.b, c:f.c})) > 0) {
-      f.color.setHex(aliveColor);
-    } else {
-      f.color.setHex(deadColor);
-    }
-  });
-  mesh.geometry.colorsNeedUpdate = true;
-}
-
-// Game of life
-
-function step(){
-  if (cells === undefined) {
-    cells = blankCells(createCells(mesh.geometry.faces));
-  }
-  if (neighborsCache === undefined) {
-    neighborsCache = Immutable.Map();
-  }
-  var faces = mesh.geometry.faces;
-  var neighbors = neighborsCache.get(faces.length)
-  if (neighbors === undefined) {
-    neighbors = createNeighbors(createCells(faces));
-    neighborsCache = neighborsCache.set(faces.length, neighbors);
-  }
-
-  var rules = [1,2,0];
-
-  cells = stepCells(cells, neighbors, rules);
-  return cells;
-}
-
-var Coord3 = Immutable.Record({a:0, b:0, c:0});
-
-function createCells(faces) {
-  return Immutable.List(
-    faces.map((face) => {
-      return new Coord3({a: face.a, b: face.b, c: face.c});
-    }));
-}
-
-function blankCells(cells) {
-  return Immutable.OrderedMap(cells.map(x => [x, 0]));
-}
-
-function randomCells(cells) {
-  return Immutable.OrderedMap(cells.map(x => [x, Math.floor(2*Math.random())]));
-}
-
-function createNeighbors(cells) {
-  return Immutable.OrderedMap(
-    cells.map(cell => [cell, neighborCells(cell, cells, 2)]));
-}
-
-function neighborCells(cell, allCells,  cutoff){
-  // cutoff is the number of shared cords
-  // 1 shared is for inderect neighbors
-  // 2 shared is for direct neighbors
-  return allCells.map(neighborCell => {
-    var commonCoords = neighborCell
-    .valueSeq()
-    .filter(value => cell.contains(value))
-    .count();
-    if (commonCoords >= cutoff &&
-        commonCoords < 3) {
-      return neighborCell;
-    } else {
-      return false;
-    }})
-  .filterNot(x => x === false)
-}
-
-function stepCells(state, neighbors, rules) {
-/*takes the current state of the board, a neighbors map and the rules (as
- * [loneliness, stasis, birth]) and returns the next state
-**/
-  return state.mapEntries(([cell, status]) => {
-    return [cell, stepCell(cell, status, state, neighbors, rules)];
-  });
-}
-
-function stepCell(cell, status, state, neighbors, rules){
-  var aliveNeighbors = neighbors.get(cell)
-  .map(neighbor => state.get(neighbor))
-  .reduce((x,y) => x + y);
-  return applyRules(status, aliveNeighbors, rules);
-}
-
-function applyRules(status, aliveNeighbors, rules) {
-  if (status > 0) { //alive
-    if (aliveNeighbors < rules[0]) {
-      return 0;
-    } else if (aliveNeighbors < rules[1]) {
-      return 1;
-    } else {
-      return 0;
-    }
-  } else { //dead
-    if (aliveNeighbors == rules[2]){
-      return 1;
-    } else {
-      return 0;
-    }
-  }
-}
-
-window.neighborsCache = neighborsCache;
-window.cells = cells;
 window.step = step;
-window.stepCells = stepCells;
 window.scene = scene;
 window.mesh = mesh;
 window.paint = paint;
-window.paintRandom = paintRandom;
-window.createNeighbors = createNeighbors;
-window.createCells = createCells;
-window.blankCells = blankCells;
-window.randomCells = randomCells;
+
+window.initGol = initGol;
+window.stop = stop;
+window.run = run;
+window.random = random;
+
 window.THREE = THREE;
 window.Immutable = Immutable;
